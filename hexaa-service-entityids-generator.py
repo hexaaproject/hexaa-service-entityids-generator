356d256ea6b6020b5cb5c64eb0d5adca6b1b7c88
Szabó Gyula <szabo.gyula@sztaki.mta.hu>
2018-05-18 11:27:47 +0200

    correct list output, PEP8

--- hexaa-service-entityids-generator.py
+++ hexaa-service-entityids-generator.py
@@ -7,44 +7,8 @@ import sys
 from re import sub
 from os import environ
 
-class MetadataHarvester:
-    def __init__(self, url):
-        filehandle = urlopen(url)
-        self.xml = filehandle.read()
-        filehandle.close()
 
-
-class Parser():
-    def __init__(self, xml):
-        document_object = parseString(xml)
-        self.parameters = []
-        for entity_descriptor in document_object.getElementsByTagName("md:EntityDescriptor"):
-            entity_id = entity_descriptor.attributes["entityID"].value
-            contacts = []
-            for contact_person_node in entity_descriptor.getElementsByTagName("md:ContactPerson"):
-                contact_type = contact_person_node.attributes["contactType"].value.encode('utf-8')
-                contact = {}
-                contact["type"] = contact_type
-                for type in [{"md:EmailAddress": "email"}, {"md:GivenName": "surName"}, {"md:SurName": "surName"}]:
-                    node = contact_person_node.getElementsByTagName(type.keys()[0])
-                    if node.length:
-                        key = type.values()[0].encode('utf-8')
-                        value = node[0].firstChild.nodeValue.encode('utf-8')
-                        if key == "email":
-                            value = sub('^mailto:', '', value)
-                        contact[key] = value
-                contacts.append(contact)
-            self.parameters.append({entity_id.encode('utf-8'): contacts})
-
-
-class Exporter():
-    def __init__(self, parameters, target_file_path):
-        export = {"parameters": {"hexaa_service_entityids": parameters}}
-        with open(target_file_path, 'w') as yaml_file:
-            safe_dump(export, yaml_file, default_flow_style=False, allow_unicode=True)
-
-
-class ConfigurationChecker():
+class ConfigurationChecker:
     def __init__(self):
         try:
             environ["TARGET_FILE_PATH"]
@@ -58,6 +22,48 @@ class ConfigurationChecker():
             sys.exit(2)
 
 
+class MetadataHarvester:
+    def __init__(self, url):
+        filehandle = urlopen(url)
+        self.xml = filehandle.read()
+        filehandle.close()
+
+
+class Parser:
+    def __init__(self, xml):
+        document_object = parseString(xml)
+        self.parameters = dict()
+        for entity_descriptor in document_object.getElementsByTagName("md:EntityDescriptor"):
+            entity_id = entity_descriptor.attributes["entityID"].value
+            contacts = []
+            for contact_person_node in entity_descriptor.getElementsByTagName("md:ContactPerson"):
+                contact_type = contact_person_node.attributes["contactType"].value.encode('utf-8')
+                contact = dict()
+                contact["type"] = contact_type
+                for attribute_type in [
+                        {"md:EmailAddress": "email"},
+                        {"md:GivenName": "surName"},
+                        {"md:SurName": "surName"}
+                        ]:
+                    node = contact_person_node.getElementsByTagName(attribute_type.keys()[0])
+                    if node.length:
+                        key = attribute_type.values()[0].encode('utf-8')
+                        value = node[0].firstChild.nodeValue.encode('utf-8')
+                        if key == "email":
+                            value = sub('^mailto:', '', value)
+                        contact[key] = value
+                contacts.append(contact)
+            self.parameters.update({entity_id.encode('utf-8'): contacts})
+
+
+class Exporter:
+    def __init__(self, parameters, target_file_path):
+        export = {"parameters": {"hexaa_service_entityids": parameters}}
+        with open(target_file_path, 'w') as yaml_file:
+            safe_dump(export, yaml_file, default_flow_style=False, allow_unicode=True)
+        # print safe_dump(export, default_flow_style=False, allow_unicode=True)
+
+
 if __name__ == "__main__":
     target_file_path = environ["TARGET_FILE_PATH"]
     metadata_sources = environ["METADATA_SOURCE_URLS"].split(",")
@@ -65,10 +71,10 @@ if __name__ == "__main__":
         print 'There is no metadata sources url in METADATA_SOURCE_URLS environment variable'
         sys.exit(2)
 
-    parameters = []
+    parameters = dict()
     for metadata_source in metadata_sources:
         mh = MetadataHarvester(metadata_source.strip())
         parser = Parser(mh.xml)
-        parameters.extend(parser.parameters)
+        parameters.update(parser.parameters)
 
     Exporter(parameters, target_file_path)
